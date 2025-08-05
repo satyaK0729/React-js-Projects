8c3e85b1a23b0a80f9449208c0fc87ab50907647
satyaK0729 <satyahi729@gmail.com>
2025-08-05 18:57:07 +0530

    Added few attributes

--- React_movie/frontend/src/pages/Home.jsx
+++ React_movie/frontend/src/pages/Home.jsx
@@ -7,6 +7,10 @@ function Home() {
         {id:1,title:"Salaar", release_date:"2023"},
         {id:2,title:"F1", release_date:"2025"},
         {id:3,title:"Avatar", release_date:"2022"},
+        {id:4,title:"Oppenheimer", release_date:"2023"},
+        {id:5,title:"Dune ", release_date:"2021"},
+        {id:6,title:"Dune 2", release_date:"2023"},
+        {id:7,title:"Fight Club", release_date:"1999"},
     ];
     const handleSearch=()=>{
         e.preventDefault();
