8764073b0daa969257c23dcc012b89cf1d9fb5df
satyaK0729 <satyahi729@gmail.com>
2025-08-11 23:24:07 +0530

    updated routes

--- React_movie/frontend/src/App.jsx
+++ React_movie/frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
-// import { Route,Routes } from 'react-router-dom'
+import { Route,Routes } from 'react-router-dom'
 import Home from "./pages/Home.jsx"
 import Favorites from './pages/Favorites.jsx'
 import AboutMe from './pages/AboutMe.jsx'
 
 function App() {
   return (
-    // <main className='main-content'>
-    //   <Routes>
-    //     <Route path='/' element={<Home/>}/>
-    //     <Route path='/Favorites' element={<Favorites/>}/>
-    //     <Route path='/About Me' element={<AboutMe/>}/>
-    //   </Routes>
-    // </main>
-
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Favorites' element={<Favorites/>}/>
+        <Route path='/About Me' element={<AboutMe/>}/>
+      </Routes>
+    </main>
     <Home/>
   );
 
